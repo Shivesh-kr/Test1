e184ffbd99b33842ff295b638671bbe245d42e51
Shivesh Kumar <69517263+Shivesh-kr@users.noreply.github.com>
2022-10-07 18:29:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.use('/',require('./routes'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.listen(port, function(err){
+        if(err){
+            console.log(`Error:${err}`);
+        }
+
+        console.log(`server is running on port:${port}`);
+
+});
